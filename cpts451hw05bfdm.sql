cc5a3cf9879c0645596924d58a04de72d6fba11a
Benjamin23115 <benjaminfarias313@gmail.com>
2024-04-24 19:32:41 -0700

    initial commit

--- cpts451hw05bfdm.sql
+++ cpts451hw05bfdm.sql
@@ -5,6 +5,9 @@
 -- Operating System: MS Windows
 
 -- Q1
+
+use CptS451_SecretProject_RDS;
+
 DELIMITER //
 create function fn_PhoneFormat_bfdm (phoneNumber varchar(255))
 returns varchar(10)
@@ -25,8 +28,6 @@ delimiter ;
 
 -- Q1 test cases
 
-use CptS451_SecretProject_RDS;
-
 select fn_PhoneFormat_bfdm ('1982736450');
 select fn_PhoneFormat_bfdm ('198.273.6450');
 select fn_PhoneFormat_bfdm ('198-273-6450');
@@ -34,247 +35,312 @@ select fn_PhoneFormat_bfdm ('(509) 335-3564');
 select fn_PhoneFormat_bfdm ('123-4567');
 select fn_PhoneFormat_bfdm ('1234567890123');
 select fn_PhoneFormat_bfdm ('Washington');
-SELECT FN_PHONEFORMAT_BFDM('NULL');
+select fn_PhoneFormat_bfdm('NULL');
 
--- Q2 
-
+-- Q2
+use CptS451_SecretProject_RDS;
 DELIMITER //
-create procedure sp_InsertSubjectX_bfdm (in fullName varchar(255), in fullAddress varchar(255), out SubFname boolean, out SubLname boolean, out SubAddress boolean, out SubCity boolean, out SubState boolean, out subZip boolean)
-BEGIN
-declare whitespace int;
+create procedure sp_InsertSubjectX_bfdm (
+    in fullName varchar(255),
+    in fullAddress varchar(255),
+    out SubFname boolean,
+    out SubLname boolean,
+    out SubAddress boolean,
+    out SubCity boolean,
+    out SubState boolean,
+    out SubZip boolean
+)
+begin
+    declare whitespace int;
+    declare firstName varchar(255);
+    declare lastName varchar(255);
+    declare address varchar(255);
+    declare city varchar(255);
+    declare state varchar(2);
+    declare zip varchar(5);
 
-declare firstName varchar(255);
-declare lastName varchar(255);
+    set whitespace = LOCATE(' ', fullName);
+    if whitespace > 0 then
+        -- Separate the complete string into substrings
+        set firstName = SUBSTRING(fullName, 1, whitespace - 1);
+        set lastName = SUBSTRING(fullName, whitespace + 1);
 
-declare address varchar(255);
-declare city varchar(255);
-declare state varchar(2);
-declare zip varchar(5);
+        -- validate if the individual parts are populated and are not null
+        set SubFname = (LENGTH(firstName) > 0 and firstName is not null);
+        set SubLname = (LENGTH(lastName) > 0 and lastName is not null);
+    end if;
 
-set whitespace = LOCATE(' ', fullName);
-if whitespace > 0 then
--- Separate the complete string into substrings
-set  firstName = SUBSTRING(fullName, 1, whitespace - 1);
-set lastName = SUBSTRING(fullName, whitespace + 1);
--- validate if the individual parts are populated and are not null
-if LENGTH(firstName) > 0 and firstName != null then
-set SubFname = true;
-else set SubFname = false;
-end if;
-if LENGTH(lastName) > 0 and lastName != null then
-set SubLname = true;
-else set SubLname = false;
-end if;
+    -- Extract address parts
+    set address = SUBSTRING_INDEX(fullAddress, ':', 1);
+    set city = SUBSTRING_INDEX(SUBSTRING_INDEX(fullAddress, ':', 2), ':', -1);
+    set state = SUBSTRING_INDEX(SUBSTRING_INDEX(fullAddress, ':', 3), ':', -1);
+    set zip = SUBSTRING_INDEX(SUBSTRING_INDEX(fullAddress, ':', 4), ':', -1);
 
---  turn the fullAddress into individual parts
-set address = substring_index2(fullAddress, ':', 1);
-set city = substring_index2(substring_index2(fullAddress, ':', 2), ':', -1);
-set state = substring_index2(substring_index2(fullAddress, ':', 3), ':', -1);
-set zip = substring_index2(substring_index2(fullAddress, ':', 4), ':', -1);
-
--- validate if the individual parts are populated and are not null
-
-if LENGTH(address) > 0 and address != null then
-set SubAddress = true;
-else set SubAddress = false;
-end if;
-
-if LENGTH(city) > 0 and city != null then
-set SubCity = true;
-else set SubCity = false;
-end if;
-
-
-if LENGTH(state) = 2 and state != null then
-set SubState = true;
-else set SubState = false;
-end if;
-
-
-if LENGTH(zip) = 5 and zip != null then
-set SubZip = true;
-else set SubZip = false;
-end if;
-
-
-end if;
-END//
+    -- Validate address parts
+    set SubAddress = (LENGTH(address) > 0 and address is not null);
+    set SubCity = (LENGTH(city) > 0 and city is not null);
+    set SubState = (LENGTH(state) = 2 and state is not null);
+    set SubZip = (LENGTH(zip) = 5 and zip is not null);
+end//
 DELIMITER ;
 
+-- Q2 extension
+DELIMITER //
+
+create function fn_InsertSubjectX_bfdm (
+    fullName varchar(255),
+    fullAddress varchar(255)
+)
+returns int
+deterministic
+begin
+    declare SubFname boolean;
+    declare SubLname boolean;
+    declare SubAddress boolean;
+    declare SubCity boolean;
+    declare SubState boolean;
+    declare SubZip boolean;
+
+    call sp_InsertSubjectX_bfdm(
+        fullName,
+        fullAddress,
+        SubFname,
+        SubLname,
+        SubAddress,
+        SubCity,
+        SubState,
+        SubZip
+    );
+
+    if not SubFname then
+        return 1;
+    elseif not SubLname then
+        return 2;
+    elseif not SubAddress then
+        return 3;
+    elseif not SubCity then
+        return 4;
+    elseif not SubState then
+        return 5;
+    elseif not SubZip then
+        return 6;
+   else
+        return 0;
+    END IF;
+END//
+
+DELIMITER ;
+
+
+
+-- Q2 test cases
+
+SELECT 
+    FN_INSERTSUBJECTX_BFDM('Christy ',
+            '7340 South Jackson Hill:Peoria:IL:61602') AS ErrorCode;
+select fn_InsertSubjectX_bfdm("Geraldine Mason", "7652 Southwest Meadow Heights:Syracuse:NY:13202") AS ErrorCode;
+select fn_InsertSubjectX_bfdm("Jennifer Thomas", NULL) AS ErrorCode;
+select fn_InsertSubjectX_bfdm(NULL, "4149 Meadow Canyon:Westminster:CO:80030") AS ErrorCode;
+select fn_InsertSubjectX_bfdm("Patrick Russell", "2233 View Lane:Fishers::46037") AS ErrorCode;
+select fn_InsertSubjectX_bfdm("Rose Adams", "1849 Southeast 3rd Bypass:Jefferson City:MO:65101") AS ErrorCode;
+
+
 -- Q3
-use wsutc_cpts451_university;
+use WSUTC_CptS451_University;
+
 alter table offering
-add offLimit int,
-add offNumEnroll int;
+add column offLimit int default 10,
+add column offNumEnrolled int;
 
-delimiter //
+DELIMITER //
 
-create procedure sp_OffNumCalc_bfdm (out offRecordUpdated int)
+create procedure sp_OffNumCalc_bfdm (out updatedCount int)
 begin
     declare done boolean default false;
     declare offeringID int;
     declare currentEnrollment int;
     declare newEnrollment int;
-    declare updatedCount int default 0;
-    declare offLimit int default 10;
+    declare enrollCount int default 0;
 
     declare offering_cursor cursor for
-    select OfferingID, CurrentEnrollment
-    from offering;
+        select OfferingID, CurrentEnrollment
+        from offering;
 
     declare continue handler for not found set done = true;
 
     open offering_cursor;
 
     calc_loop: loop
-        -- Fetch offering details
         fetch offering_cursor into offeringID, currentEnrollment;
 
         if done then
             leave calc_loop;
         end if;
 
-        set newEnrollment = (select COUNT(*) from enrollment where OfferingID = offeringID);
+        set newEnrollment = (select COUNT(*) from enrollment where enrollment.OfferNo = offering.offerNo);
 
         if newEnrollment <> currentEnrollment then
-            if newEnrollment <= offLimit then
+            if newEnrollment <= (select offLimit from offering where OfferingID = offeringID) then
                 update offering
-                set OffNumEnroll = newEnrollment
+                set offNumEnrolled = newEnrollment
                 where OfferingID = offeringID;
                 
-                set updatedCount = updatedCount + 1;
+                set enrollCount = enrollCount + 1;
             end if;
         end if;
     end loop calc_loop;
 
     close offering_cursor;
-    set offRecordUpdated = updatedCount;
+    set updatedCount = enrollCount;
 end //
+
+DELIMITER ;
+
+create view vw_offNumBad_bfdm as
+select *
+from offering
+where offNumEnrolled > offLimit;
 
 -- Q3 tests
 
-INSERT INTO offering (OfferNo, CourseNo, OffTerm, OffYear, OffLocation, OffTime, FacNo, OffDays, OffLimit, OffNumEnrolled)
-VALUES (1, 'CPT451', 'Spring', 2024, 'Room A', '10:00', 'FAC101', 'MWF', 20, 0);
+use WSUTC_CptS451_University;
 
-CALL sp_OffNumCalc_bfdm(@updatedCount1);
+insert into offering (OfferNo, CourseNo, OffTerm, OffYear, OffLocation, OffTime, FacNo, OffDays, OffNumEnrolled)
+values (1112, 'IS320', 'SUMMER', 2020, 'BLM302', '10:30:00', NULL, 'MW', 0),
+       (7778, 'FIN480', 'SPRING', 2020, 'BLM305', '13:30:00', '765-43-2109', 'MW', 5),
+       (8889, 'IS320', 'SUMMER', 2020, 'BLM405', '13:30:00', '654-32-1098', 'MW', 20),
+       (9877, 'IS460', 'SPRING', 2020, 'BLM307', '13:30:00', '654-32-1098', 'TTH', 50);
 
-SELECT * FROM offering;
+call sp_OffNumCalc_bfdm(@updatedCount1);
 
-INSERT INTO offering (OfferNo, CourseNo, OffTerm, OffYear, OffLocation, OffTime, FacNo, OffDays, OffLimit, OffNumEnrolled)
-VALUES (2, 'CPT452', 'Fall', 2024, 'Room B', '09:00', 'FAC102', 'TTH', 15, 8);
-
-CALL sp_OffNumCalc_bfdm(@updatedCount2);
-
-SELECT * FROM offering;
-
-INSERT INTO offering (OfferNo, CourseNo, OffTerm, OffYear, OffLocation, OffTime, FacNo, OffDays, OffLimit, OffNumEnrolled)
-VALUES 
-(3, 'CPT453', 'Spring', 2024, 'Room C', '11:00', 'FAC103', 'MWF', 25, 18),
-(4, 'CPT454', 'Summer', 2024, 'Room D', '14:00', 'FAC104', 'TTH', 30, 24);
-
-CALL sp_OffNumCalc_bfdm(@updatedCount3);
-
-SELECT * FROM offering;
-
-INSERT INTO offering (OfferNo, CourseNo, OffTerm, OffYear, OffLocation, OffTime, FacNo, OffDays, OffLimit, OffNumEnrolled)
-VALUES 
-(5, 'CPT455', 'Fall', 2024, 'Room E', '13:00', 'FAC105', 'MWF', 20, 15),
-(6, 'CPT456', 'Spring', 2024, 'Room F', '10:00', 'FAC106', 'TTH', 25, 20);
-
-CALL sp_OffNumCalc_bfdm(@updatedCount4);
-
-SELECT * FROM offering;
+select *  from vw_offNumBad_bfdm;
 
 delimiter //
 
 -- Q4 
 
-USE CptS451_SecretProject_RDS;
+USE CptS451_SecretProjoffLimitect_RDS;
 
 DELIMITER //
 
 create trigger tr_ib_Task_bfdm before insert on Task
 for each row
 begin
-    declare errorMessage varchar(255);
 
     if NEW.TaskCost < 0 then
-        set errorMessage = 'Error: Negative data insertion not allowed.';
-        signal sqlstate '45000' set message_text = errorMessage;
+        call myFail('Negative data insertion not allowed.');
     end if;
 end;
-//
 
 create trigger tr_ub_Task_bfdm before update on Task
 for each row
 begin
-    declare errorMessage varchar(255);
 
     if NEW.TaskCost < 0 then
-        set errorMessage = 'Error: Negative data update not allowed.';
-        signal sqlstate '45000' set message_text = errorMessage;
+        call myFail('Negative data update not allowed.');
     end if;
 end;
-//
 
 DELIMITER ;
 
 -- Q4 test cases
 
-UPDATE Task SET TaskCost = 123.45 WHERE TaskNo = 7;
-UPDATE Task SET TaskCost = -43.21 WHERE TaskNo = 7;
-INSERT Task(TaskDesc, TaskType, TaskCost) VALUES('Myringotomy', 5, 33.33);
-INSERT Task(TaskDesc, TaskType, TaskCost) VALUES('Curettage', 3, -22.22);
+update Task set TaskCost = 123.45 where TaskNo = 7;
+update Task set TaskCost = -43.21 where TaskNo = 7;
+insert Task(TaskDesc, TaskType, TaskCost) values('Myringotomy', 5, 33.33);
+insert Task(TaskDesc, TaskType, TaskCost) values('Curettage', 3, -22.22);
 
 
 -- Q5
 
-use WSUTC_CptS451_OrderEntry11;
+USE WSUTC_CptS451_OrderEntry11;
 
 delimiter //
+CREATE PROCEDURE handleNoRowsAffected()
+BEGIN
+    CALL myFail('Error: Customer does not exist');
+END//
+delimiter ;
 
-CREATE TRIGGER tr_ia_Task_bfdm
-AFTER INSERT ON Order
+DELIMITER //
+
+CREATE TRIGGER tr_ia_task_bfdm AFTER INSERT ON OrdLine
 FOR EACH ROW
 BEGIN
-    UPDATE Customer
-    SET CustBal = CustBal + NEW.TotalAmt
-    WHERE CustID = NEW.CustID;
-END;
+    DECLARE prod_price DECIMAL(10, 2);
 
-CREATE TRIGGER tr_ua_Task_bfdm
-AFTER UPDATE ON Order
+    SELECT ProdPrice INTO prod_price FROM product WHERE ProdNo = NEW.ProdNo;
+
+    UPDATE customer
+    JOIN ordertbl ON customer.CustNo = ordertbl.CustNo 
+    SET customer.CustBal = customer.CustBal + (NEW.Qty * prod_price)
+    WHERE ordertbl.OrdNo = NEW.OrdNo;
+
+    IF ROW_COUNT() = 0 THEN
+        CALL handleNoRowsAffected();
+    END IF;
+END //
+
+CREATE TRIGGER tr_ua_task_bfdm AFTER UPDATE ON OrdLine
 FOR EACH ROW
 BEGIN
-    DECLARE old_total DECIMAL(10,2);
-    DECLARE new_total DECIMAL(10,2);
+    DECLARE prod_price DECIMAL(10, 2);
 
-    SELECT TotalAmt INTO old_total FROM Order WHERE OrderID = OLD.OrderID;
-    SELECT TotalAmt INTO new_total FROM Order WHERE OrderID = NEW.OrderID;
+    SELECT ProdPrice INTO prod_price FROM Product WHERE ProdNo = NEW.ProdNo;
 
-    UPDATE Customer
-    SET CustBal = CustBal - old_total + new_total
-    WHERE CustID = NEW.CustID;
-END;
+    UPDATE customer
+    JOIN ordertbl ON customer.CustNo = ordertbl.CustNo
+    JOIN ordline ON ordertbl.OrdNo = ordline.OrdNo 
+    SET customer.CustBal = customer.CustBal + ((NEW.Qty - OLD.Qty) * prod_price)
+    WHERE ordline.OrdNo = NEW.OrdNo;
 
-CREATE TRIGGER tr_da_Task_bfdm
-AFTER DELETE ON Order
+    IF ROW_COUNT() = 0 THEN
+        CALL handleNoRowsAffected();
+    END IF;
+END //
+
+CREATE TRIGGER tr_da_task_bfdm AFTER DELETE ON OrdLine
 FOR EACH ROW
 BEGIN
-    UPDATE Customer
-    SET CustBal = CustBal - OLD.TotalAmt
-    WHERE CustID = OLD.CustID;
-END;
+    DECLARE prod_price DECIMAL(10, 2);
+
+SELECT 
+    ProdPrice
+INTO prod_price FROM
+    product
+WHERE
+    ProdNo = OLD.ProdNo;
+
+UPDATE customer
+        JOIN
+    ordertbl ON customer.CustNo = ordertbl.CustNo
+        JOIN
+    ordline ON ordertbl.OrdNo = ordline.OrdNo 
+SET 
+    CustBal = CustBal - (OLD.Qty * prod_price)
+WHERE
+    ordertbl.OrdNo = OLD.OrdNo;
+
+    IF ROW_COUNT() = 0 THEN
+        CALL handleNoRowsAffected();
+    END IF;
+END //
+
+DELIMITER ;
+
 
 -- Q5 test cases
-
+delimiter //
 -- Testing INSERT trigger
-INSERT INTO Order (OrderID, CustID, TotalAmt) VALUES (1, 101, 50.00);
+INSERT INTO OrdLine (OrdNo, ProdNo, Qty) VALUES ('O9919699', 'P4200344', 1);
 
 -- Testing UPDATE trigger
-UPDATE Order SET TotalAmt = 60.00 WHERE OrderID = 1;
+UPDATE OrdLine SET Qty = 3 WHERE OrdNo = 'O9919698' AND ProdNo = 'P4200344';
 
 -- Testing DELETE trigger
-DELETE FROM Order WHERE OrderID = 1;
+DELETE FROM OrdLine WHERE OrdNo = 'O9919698' AND ProdNo = 'P4200344';
+
 
 delimiter ;
+
+
---
+++ readme.txt
@@ -0,0 +1,10 @@
+-- Washington State University: CptS451
+-- Semester: Spring 2024
+-- Student: Benjamin Farias Dela Mora
+-- Assignment: Homework 05
+-- Operating System: MS Windows
+
+
+I was going crazy trying to think of edge cases that could be handled by calling myFail so I only added edge case handling for "general" edge cases. 
+
+For Q2, this assignment assumes the zip code being inserted is a 5 digit number rather than a 9 digit number/string. For Q2, I created a function that calls the procedure the homework is asking for. This is probably not the intended or "right" way to do it, but its late, im tired and its what I thought of 😎.
